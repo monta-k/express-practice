b0461344efd9a7af3dd19ffb0d5db434fef156b5
Kobayashi Shunta <monta.k0523@gmail.com>
2019-09-06 11:52:23 +0900

    test article api and user api

--- app/routes/v1/index.js
+++ app/routes/v1/index.js
@@ -1,10 +1,7 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/', function(req, res) {
-  res.json({
-    message: 'Hello, World'
-  })
-})
+router.use('/article', require('./article.js'))
+router.use('/user', require('./user.js'))
 
 module.exports = router
